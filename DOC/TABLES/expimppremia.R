8e3aab6bf2de21bb4141fc4bf68d8ad42d2fb9df
Arjun <arjung93@gmail.com>
2020-01-12 13:47:17 +0100

    Adding DOC and PICS

---
+++ DOC/TABLES/expimppremia.R
@@ -0,0 +1,33 @@
+
+% Table created by stargazer v.5.2 by Marek Hlavac, Harvard University. E-mail: hlavac at fas.harvard.edu
+% Date and time: Tue, Nov 19, 2019 - 10:21:50  CET
+\begin{table}[!htbp] \centering 
+  \caption{Export and Import Premia} 
+  \label{expimppremia} 
+\begin{tabular}{@{\extracolsep{5pt}}lcccc} 
+\\[-1.8ex]\hline 
+\hline \\[-1.8ex] 
+ & \multicolumn{4}{c}{\textit{Dependent variable:}} \\ 
+\cline{2-5} 
+\\[-1.8ex] & Sales & Gross Fixed Assets & Raw Materials & Salary \\ 
+\\[-1.8ex] & (1) & (2) & (3) & (4)\\ 
+\hline \\[-1.8ex] 
+ exp & 0.452$^{***}$ & 0.233$^{***}$ & 0.456$^{***}$ & 0.340$^{***}$ \\ 
+  & (0.014) & (0.010) & (0.016) & (0.011) \\ 
+  & & & & \\ 
+ imp & 0.463$^{***}$ & 0.200$^{***}$ & 0.477$^{***}$ & 0.307$^{***}$ \\ 
+  & (0.011) & (0.008) & (0.013) & (0.009) \\ 
+  & & & & \\ 
+ exp:imp & $-$0.054$^{***}$ & 0.030$^{**}$ & $-$0.029 & $-$0.001 \\ 
+  & (0.016) & (0.012) & (0.018) & (0.013) \\ 
+  & & & & \\ 
+\hline \\[-1.8ex] 
+Observations & 79,949 & 79,949 & 79,949 & 79,949 \\ 
+R$^{2}$ & 0.150 & 0.179 & 0.144 & 0.253 \\ 
+Adjusted R$^{2}$ & 0.042 & 0.074 & 0.035 & 0.157 \\ 
+F Statistic (df = 23; 70865) & 545.807$^{***}$ & 672.610$^{***}$ & 520.106$^{***}$ & 1,041.458$^{***}$ \\ 
+\hline 
+\hline \\[-1.8ex] 
+\textit{Note:}  & \multicolumn{4}{r}{$^{*}$p$<$0.1; $^{**}$p$<$0.05; $^{***}$p$<$0.01} \\ 
+\end{tabular} 
+\end{table} 
